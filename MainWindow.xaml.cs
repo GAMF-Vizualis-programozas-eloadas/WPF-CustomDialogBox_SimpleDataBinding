5638bb196969e79c0319801d05ae0ab58ba1c19d
jzscsaba <johanyak.csaba@nje.hu>
2024-09-21 09:38:42 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomDialogBox
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void miExit_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void miNew_Click(object sender, RoutedEventArgs e)
+		{
+			var wnd = new wndGetAverageDlg
+			{
+				Owner = this,
+				Person =
+				{
+					Name = "Valaki Eduárd",
+					Average = 2
+				}
+			};
+			var res = wnd.ShowDialog();
+			if (res == true)
+			{
+				tbMain.Text = wnd.Person.Name + "\r\n" +
+					wnd.Person.Average;
+			}
+
+		}
+	}
+}
---
+++ wndGetAverageDlg.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomDialogBox
+{
+	public partial class wndGetAverageDlg : Window
+	{
+		public Person Person { get; set; }
+		public wndGetAverageDlg()
+		{
+			InitializeComponent();
+			Person = new Person();
+			gr.DataContext = Person;
+		}
+		private void btOK_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+	}
+}
